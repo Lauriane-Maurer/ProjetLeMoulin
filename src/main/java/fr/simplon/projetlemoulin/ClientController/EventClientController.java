281a2822038bb25770d0732919d07fbab04195c7
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-06-01 15:19:22 +0200

    Ajout commentaires javadoc Event: - EventServerController (CRUD) - EventClientController get /programmation

--- src/main/java/fr/simplon/projetlemoulin/ClientController/EventClientController.java
+++ src/main/java/fr/simplon/projetlemoulin/ClientController/EventClientController.java
@@ -2,32 +2,64 @@ package fr.simplon.projetlemoulin.ClientController;
 
 import fr.simplon.projetlemoulin.Entities.Event;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+
+/**
+ * Controller class for handling client-side event-related requests.
+ *
+ * This class acts as a controller for the client-side application and handles
+ * requests related to events.
+ *
+ */
 
 @Controller
 public class EventClientController {
 
     private RestTemplate restTemplate;
+
+    /**
+     * Retrieves the "programmation" page.
+     *
+     * This method handles the GET request for the "/programmation" endpoint and
+     * returns the page "programmation". It is responsible for displaying
+     * the "programmation" page.
+     *
+     * @return The view name for the "programmation" page.
+     */
+
     @GetMapping("/programmation")
-    public String getAllEvents(Model model) {
-        this.restTemplate = new RestTemplate();
-        String url = "http://localhost:8085/rest/events";
-        ResponseEntity<List<Event>> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Event>>() {
-                });
-        List<Event> events = response.getBody();
-        model.addAttribute("events", events);
+    public String getProgrammationPage() {
         return "programmation";
+    }
+
+    /**
+     * Retrieves all events from the API endpoint.
+     *
+     * This method handles the GET request for the "/cartesEvenements" endpoint and retrieves
+     * all event from the API. It uses RestTemplate to send an HTTP GET request to the
+     * API endpoint "http://localhost:8085/rest/eventsCards". The response, containing a list
+     * of events, is then returned as the result to be displayed in card format using the
+     * "EventCard" javascript file.
+     *
+     * @return A list of events retrieved from the API.
+     */
+    @GetMapping("/cartesEvenements")
+    @ResponseBody
+    public List<Event> getAllEventsCards() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        ResponseEntity<List<Event>> response = restTemplate.exchange("http://localhost:8085/rest/eventsCards", HttpMethod.GET, entity, new ParameterizedTypeReference<List<Event>>() {
+        });
+        return response.getBody();
     }
 
 
--- src/main/java/fr/simplon/projetlemoulin/ServerController/EventServerController.java
+++ src/main/java/fr/simplon/projetlemoulin/ServerController/EventServerController.java
@@ -4,10 +4,18 @@ import fr.simplon.projetlemoulin.Entities.Event;
 import fr.simplon.projetlemoulin.Repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
+
+
+/**
+ * RestController class for managing events.
+ *
+ * This class serves as the REST API controller for handling event-related operations.
+ * It provides endpoints for retrieving, adding, updating, and deleting events (CRUD).
+ *
+ * @RestController indicates that this class is a controller that handles RESTful requests
+ */
 
 @RestController
 public class EventServerController {
@@ -23,14 +31,23 @@ public class EventServerController {
         //this.repo.save(new Event("Stage", "Fresque de la biodiversité","Fresque de la biodiversité, kesako? Un atelier collaboratif pour mieux comprendre le rôle des écosystèmes et imaginer ensemble comment inverser les tendances de leurs effondrement. Une expérience d’intelligence collective qui parle autant à la tête qu’au coeur.", LocalDateTime.of(2023, 8, 9, 10,00), LocalDateTime.of(2023, 8, 9, 17, 00),true, 10, 10, 5.00, "Nathalie Richard", "https://picsum.photos/id/290/150.webp",null));
     }
 
-
+    /**
+     * Retrieves all events from the repository.
+     *
+     * @return A list of all events.
+     */
     @GetMapping(path = "/rest/events")
     public List<Event> getEvent() {
         return repo.findAll();
     }
 
 
-    @GetMapping("/rest/api/events")
+    /**
+     * Retrieves all events as JSON from the repository.
+     *
+     * @return A list of all events in JSON format.
+     */
+    @GetMapping("/rest/eventsCards")
     @ResponseBody
     public List<Event> getAllEventsAsJson() {
         List<Event> events = repo.findAll();
@@ -38,18 +55,37 @@ public class EventServerController {
     }
 
 
+    /**
+     * Retrieves details of a specific event by its ID.
+     *
+     * @param id The ID of the event to retrieve.
+     * @return The event with the specified ID.
+     * @throws NoSuchElementException if the event with the given ID is not found.
+     */
     @GetMapping(path = "/rest/events/{id}")
     public Event getEventDetails(@PathVariable Long id)throws NoSuchElementException {
         return repo.findById(id).orElseThrow();
     }
 
 
+    /**
+     * Adds a new event to the repository.
+     *
+     * @param newEvent The event to add.
+     * @return The newly added event.
+     */
     @PostMapping(path="/rest/events")
     public Event addEvent(@RequestBody Event newEvent) {
         return repo.save(newEvent);
     }
 
-
+    /**
+     * Updates an existing event in the repository.
+     *
+     * @param newEvent The updated event data.
+     * @param id The ID of the event to update.
+     * @return The updated event.
+     */
     @PostMapping("/rest/UpdateEvent/{id}")
     public Event updateEvent(@RequestBody Event newEvent, @PathVariable Long id) {
         return repo.findById(id)
@@ -57,8 +93,8 @@ public class EventServerController {
                     event.setType(newEvent.getType());
                     event.setTitle(newEvent.getTitle());
                     event.setDescription(newEvent.getDescription());
-                    event.setStartDate(newEvent.getStartDate());
-                    event.setEndDate(newEvent.getEndDate());
+                    event.setStart_date(newEvent.getStart_date());
+                    event.setEnd_date(newEvent.getEnd_date());
                     event.setLimit_places(newEvent.isLimit_places());
                     event.setTotal_places(newEvent.getTotal_places());
                     event.setAvailable_places(newEvent.getAvailable_places());
@@ -74,6 +110,15 @@ public class EventServerController {
                 });
     }
 
+
+    /**
+     * Deletes an event from the repository.
+     *
+     * This method handles the DELETE request for the "/rest/events/{id}" endpoint and
+     * deletes the event with the specified ID from the repository.
+     *
+     * @param id The ID of the event to delete.
+     */
     @DeleteMapping("/rest/events/{id}")
     public void deleteEvent(@PathVariable Long id) {
         repo.deleteById(id);
