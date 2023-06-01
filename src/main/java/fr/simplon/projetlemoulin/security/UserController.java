ffed6cc19f502dbe52a1116fbffce041ee1cea5e
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-06-01 12:25:29 +0200

    Ajout modification Mot de passe: - page modificationMdp dans dossier "connexion" - Ajout lien dans navbar - Ajout classe ChangePasswordForm Dans classe UserController: - Methode get /ModificationMdp - Methode post /ModificationMdp Ajout page message.html Ajout style message.css

--- src/main/java/fr/simplon/projetlemoulin/security/UserController.java
+++ src/main/java/fr/simplon/projetlemoulin/security/UserController.java
@@ -1,5 +1,6 @@
 package fr.simplon.projetlemoulin.security;
 
+import fr.simplon.projetlemoulin.dto.ChangePasswordForm;
 import fr.simplon.projetlemoulin.dto.CreateUserForm;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.sql.DataSource;
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -41,11 +43,22 @@ public class UserController {
         userDetailsManager = pUserDetailsManager;
     }
 
+
+    /**
+     * Access to log in
+     * @return direct to the login page in the connexion file.
+     */
+
     @GetMapping("/login")
     String login() {
-        return "login";
+        return "connexion/login";
     }
 
+
+    /**
+     * Logs out the currently authenticated user and invalidates the session.
+     * @return A redirection to the home page.
+     */
 
     @GetMapping(path = "/deconnexion")
     public String logout(HttpServletRequest request) {
@@ -58,12 +71,38 @@ public class UserController {
     }
 
 
+
+    /**
+     * Displays the user registration page.
+     *
+     * Handles the GET request for the "/inscription" endpoint and returns the registration form
+     * "inscriptionMembre" in the connexion file.
+     *
+     * @param model The model object for passing data to the view.
+     * @return The registration form.
+     */
+
     @GetMapping("/inscription")
     public String subscribe(Model model) {
         model.addAttribute("user", new CreateUserForm());
-        return "inscriptionMembre";
+        return "connexion/inscriptionMembre";
     }
 
+    /**
+     * Handles user registration form submission.
+     *
+     * Processes the POST request for the "/inscription" endpoint and validates the submitted
+     * user registration form. If there are validation errors, returns the view name
+     * "/connexion/inscriptionMembre" to display the form with error messages. Otherwise,
+     * creates a new user account with the encoded password and "USER" role in the database,
+     * and redirects to the login page.
+     *
+     * @param user       The submitted CreateUserForm object.
+     * @param validation The BindingResult object for validation errors.
+     * @param model      The model object for passing data to the view.
+     * @return The user registration form in case of validation errors,
+     *         otherwise a redirection to the login page.
+     */
     @PostMapping("/inscription")
     @Transactional
     public String subscribe(
@@ -78,16 +117,61 @@ public class UserController {
             validation.addError(new ObjectError("user", "Cet utilisateur existe déjà"));
         }
         if (validation.hasErrors()) {
-            return "/inscriptionMembre";
+            return "/connexion/inscriptionMembre";
         }
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         // Roles for new user
         Collection<? extends GrantedAuthority> roles = Arrays.asList(new SimpleGrantedAuthority("USER"));
         UserDetails userDetails = new User(user.getLogin(), encodedPassword, roles);
-        // Create the account in database with all its roles
+        // Create the account in database with its role
         userDetailsManager.createUser(userDetails);
-        return "redirect:/login";
+        return "redirect:/connexion/login";
     }
 
+
+
+    @GetMapping("/modificationMdp")
+    public String changePassword(Model model)
+    {
+        ChangePasswordForm userForm = new ChangePasswordForm();
+        model.addAttribute("user", userForm);
+        return "connexion/modificationMdp";
+    }
+
+
+    @PostMapping("/modificationMdp")
+    public String changePassword(
+            Principal principal,
+            @ModelAttribute("user") @Valid ChangePasswordForm user,
+            BindingResult validation,
+            Model model) {
+        UserDetails userDetails = userDetailsManager.loadUserByUsername(principal.getName());
+        changePassword(userDetails, user, validation);
+        if (validation.hasErrors()) {
+            return "connexion/modificationMdp";
+        } else
+        {
+            model.addAttribute("Message", "Votre mot de passe a été modifié avec succès !");
+            return "message";
+        }
+    }
+
+    private void changePassword(UserDetails userDetails, ChangePasswordForm form, BindingResult validation) {
+        if (userDetails == null) {
+            validation.addError(new FieldError("user", "username", "Utilisateur inconnu"));
+        } else {
+            String currentPassword = userDetails.getPassword();
+
+            if (!passwordEncoder.matches(form.getOldPassword(), currentPassword)) {
+                validation.addError(new FieldError("user", "oldPassword", "Mot de passe invalide"));
+            }
+            if (!form.getNewPassword().equals(form.getConfirmPassword())) {
+                validation.addError(new FieldError("user", "confirmPassword", "Les mots de passe ne correspondent pas"));
+            }
+            if (!validation.hasErrors()) {
+                userDetailsManager.changePassword(form.getOldPassword(), passwordEncoder.encode(form.getNewPassword()));
+            }
+        }
+    }
 
 }
