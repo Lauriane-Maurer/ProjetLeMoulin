8860a307e032640cdebab6b5626dc3324a08fda3
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-06-02 13:36:32 +0200

    Ajout bouton "supprimer" dans listeEvenementsAdmin.html: - Ajout confirmDelationEvent dans popUpConfirmation.js Dans EvenetClientController: - Ajout méthode Get /DeleteEvent

--- src/main/resources/static/javascript/popUpConfirmation.js
+++ src/main/resources/static/javascript/popUpConfirmation.js
@@ -2,7 +2,12 @@ function confirmDeletion(id) {
     if (confirm("Êtes-vous sûr de vouloir supprimer cette inscription ?")) {
         window.location.href = '/annulationParticipation/' + id;
     }
+}
 
 
+    function confirmDeletionEvent(id) {
+        if (confirm("Êtes-vous sûr de vouloir supprimer cet évènement ?")) {
+            window.location.href = '/supprimerEvenement/' + id;
+        }
 }
 
